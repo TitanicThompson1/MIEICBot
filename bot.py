e4fea1437a0a255b0d6e40905f154f7b46f13a19
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2020-12-18 21:50:35 -0100

    Added AllExams command

--- bot.py
+++ bot.py
@@ -12,18 +12,32 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 
-@bot.command(name='nextExam', help='Responds with the next exam')
+@bot.command(name='nextExam', help=' [discipline] Gets with the next exam')
 async def nextExam(ctx, discipline: str = None):
-    print
-    conn = init_database()
+    conn = connect_database()
     
     server_resp = get_next_exam(conn, discipline)
     if server_resp == "Error":
         raise commands.CommandError()
 
-    await send_response_exam(ctx, server_resp, discipline != None)
+    await send_resp_nextexam(ctx, server_resp, discipline != None)
     
     close_db(conn)
+
+
+@bot.command(name='allExams', help='Responds with the next exam')
+async def allExams(ctx, discipline: str = None):
+    conn = connect_database()
+    
+    server_resp = get_all_examss(conn, discipline)
+    if server_resp == "Error":
+        raise commands.CommandError()
+    
+
+    await send_resp_allexams(ctx, server_resp, discipline != None)
+    
+    close_db(conn)
+
 
 
 @nextExam.error
--- dbfunctions.py
+++ dbfunctions.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def init_database():
+def connect_database():
     return sqlite3.connect('test.db')
 
 
@@ -15,8 +15,32 @@ def get_next_exam(conn, discipline: str):
     if not _discipline_exists(c, discipline):
         return "Error"
 
-    return _specific_exam(c, discipline)
+    return _next_exam_dscpl(c, discipline)
 
+
+
+def get_all_examss(conn, discipline: str):
+    c = conn.cursor()
+
+    if discipline == None:
+        return _all_exams(c)
+    
+    if not _discipline_exists(c, discipline):
+        return "Error"
+
+    return _all_examss_dscpl(c, discipline)
+
+
+def _all_exams(c):
+    c.execute('''
+    SELECT name, date 
+    FROM Exam, Discipline
+    WHERE Exam.idDiscipline=Discipline.idDiscipline
+    ORDER BY date(date) 
+    ASC;
+    ''')
+    
+    return c.fetchall()
 
 
 def _discipline_exists(c, discipline):
@@ -33,7 +57,26 @@ def _discipline_exists(c, discipline):
     return c.fetchone() != None
 
 
-def _specific_exam(c, discipline):
+def _all_examss_dscpl(c, discipline):
+    dscp_upper =discipline.upper()
+
+    tuple_querry = (dscp_upper,)
+
+    c.execute('''
+    SELECT name, date 
+    FROM Exam, Discipline
+    WHERE Exam.idDiscipline=Discipline.idDiscipline AND
+            Discipline.name=?
+    ORDER BY date(date) 
+    ASC;
+    ''', tuple_querry)
+
+    return c.fetchall()
+
+
+
+
+def _next_exam_dscpl(c, discipline):
     dscp_upper =discipline.upper()
 
     tuple_querry = (dscp_upper,)
