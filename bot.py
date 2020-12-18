aa40acf767c7d08a8370c786c99c59f8381be1bc
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2020-12-18 14:58:21 -0100

    Created database

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
-# bot.py
 import os
+import random
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-client.run(TOKEN)
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(TOKEN)
